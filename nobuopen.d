3a9636dfef8aabb2a6416dd3d1c3c9027435efd7
Jorge Velázquez <jorge.velazquez@gmail.com>
2012-02-11 18:08:20 -0800

    removing product-specific traces that i no longer need

--- nobuopen.d
+++
@@ -1,7 +0,0 @@
-#!/usr/sbin/dtrace -s
-
-syscall::open:entry /execname=="nobud"/
-{
-	printf("%s %s",execname,copyinstr(arg0));
-}
-
--- pelecheck.d
+++
@@ -1,7 +0,0 @@
-#!/usr/sbin/dtrace -s
-
-pid$1::RapidPoller??PollLoop():720
-{
-	printf("%Y: %s %s", walltimestamp, execname, copyinstr(arg0));
-}
-
