d177ba37df263dd827cfc6d94754e1460d6b948a
Jorge Velázquez <jorge.velazquez@proximaldata.com>
2012-03-06 10:42:01 -0800

    modified script to output name and also to filter on different process

--- clay.d
+++ clay.d
@@ -8,19 +8,19 @@
 
 dtrace:::BEGIN
 {
-	printf("O %12s %8s\n", "OFFSET", "COUNT");
+	printf("O %12s %8s %s\n", "OFFSET", "COUNT", "FILENAME");
 }
 
 fbt::hfs_vnop_read:entry
-/execname == "test"/
+/execname == "pdiFilterTest"/
 {
 	this->read = (struct vnop_read_args *)arg0;
-	printf("R %12d %8d\n", this->read->a_uio->uio_offset, this->read->a_uio->uio_resid_64);
+	printf("R %12d %8d %s\n", this->read->a_uio->uio_offset, this->read->a_uio->uio_resid_64, stringof(this->read->a_vp->v_name));
 }
 
 fbt::hfs_vnop_write:entry
-/execname == "test"/
+/execname == "pdiFilterTest"/
 {
 	this->write = (struct vnop_write_args *)arg0;
-	printf("W %12d %8d\n", this->write->a_uio->uio_offset, this->write->a_uio->uio_resid_64);
+	printf("W %12d %8d %s\n", this->write->a_uio->uio_offset, this->write->a_uio->uio_resid_64, stringof(this->write->a_vp->v_name));
 }
