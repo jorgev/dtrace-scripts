c1386c340bdb2006f6d716d1cbc05ca3bcb050af
Jorge Velázquez <jorge.velazquez@gmail.com>
2012-03-05 23:55:24 -0800

    right-justified the output and added a header

--- clay.d
+++ clay.d
@@ -6,15 +6,21 @@
 
 #pragma D option quiet
 
+dtrace:::BEGIN
+{
+	printf("O %12s %8s\n", "OFFSET", "COUNT");
+}
+
 fbt::hfs_vnop_read:entry
 /execname == "test"/
 {
 	this->read = (struct vnop_read_args *)arg0;
-	printf("R %d %d\n", this->read->a_uio->uio_offset, this->read->a_uio->uio_resid_64);
+	printf("R %12d %8d\n", this->read->a_uio->uio_offset, this->read->a_uio->uio_resid_64);
 }
+
 fbt::hfs_vnop_write:entry
 /execname == "test"/
 {
 	this->write = (struct vnop_write_args *)arg0;
-	printf("W %d %d\n", this->write->a_uio->uio_offset, this->write->a_uio->uio_resid_64);
+	printf("W %12d %8d\n", this->write->a_uio->uio_offset, this->write->a_uio->uio_resid_64);
 }
